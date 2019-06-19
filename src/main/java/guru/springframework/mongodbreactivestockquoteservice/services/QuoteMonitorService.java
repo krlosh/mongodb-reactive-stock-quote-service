9140f7cf0fa2a63162c0e838170becbba138715a
krlosh <krlosh@gmail.com>
2019-06-19 13:02:53 +0200

    Added service to load data into mongo

---
+++ src/main/java/guru/springframework/mongodbreactivestockquoteservice/services/QuoteMonitorService.java
@@ -0,0 +1,31 @@
+package guru.springframework.mongodbreactivestockquoteservice.services;
+
+import guru.springframework.mongodbreactivestockquoteservice.client.StockQuoteClient;
+import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
+import guru.springframework.mongodbreactivestockquoteservice.repositories.QuoteRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class QuoteMonitorService implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final StockQuoteClient stockQuoteClient;
+    private final QuoteRepository quoteRepository;
+
+    public QuoteMonitorService(StockQuoteClient stockQuoteClient, QuoteRepository quoteRepository) {
+        this.stockQuoteClient = stockQuoteClient;
+        this.quoteRepository = quoteRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        this.stockQuoteClient.getQuoteStream()
+                .log("quote-monitor-service")
+                .subscribe(quote -> {
+                    Mono<Quote> savedQuote = this.quoteRepository.save(quote);
+                    System.out.println("I saved a quote, id:"+savedQuote.block().getId());
+                });
+    }
+}
